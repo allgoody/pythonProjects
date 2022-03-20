93541be3853701d9bf71da9f2b64bf5f33a3c815
allgoody <allgoodiesnet@gmailcom>
2022-03-20 20:20:03 +0200

    moving to basics

--- basics/app3.py
+++ basics/app3.py
@@ -1,6 +1,5 @@
-# This first line is provided for you
-
+# calculating pay
 hrs = input("Enter Hours:")
 rate = input("Enter Pay Rate:")
-pay = float(hrs)*float(rate)
+pay = float(hrs) * float(rate)
 print("Pay:",pay)
