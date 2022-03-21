0c957db446d8762a0ad4c89db3e7cbf58770c229
allgoody <allgoodiesnet@gmailcom>
2022-03-21 22:21:08 +0200

    new

--- basics/app7_try_except.py
+++ basics/app7_try_except.py
@@ -4,3 +4,17 @@
 # if try fails  - it jumps to except section
 
 
+astr = input('input something:')
+try:
+    istr = int(astr)    # traceback here
+except:
+    istr = -1
+
+print('first', istr)
+
+astr = input('input something again:')
+try:
+    istr = int(astr)
+except:
+    istr = -2
+print('second', istr)
