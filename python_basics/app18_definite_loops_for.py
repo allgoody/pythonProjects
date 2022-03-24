3ca2a164351e9f9194e080fa4daf6b654124fffb
allgoody <allgoodiesnet@gmailcom>
2022-03-24 02:07:04 +0200

    something new

--- python_basics/app18_definite_loops_for.py
+++ python_basics/app18_definite_loops_for.py
@@ -1,5 +1,11 @@
 # for - definite loops
 
-for i in [5, 4, 3, 2, 1] :
+num = [5, 4, 3, 2, 1]
+for i in num :
     print(i)
 print('blastoff!')
+
+friends = ['Josef', 'Glenn', 'Sally', 'Dan', 'Tata']
+for friend in friends :
+    print('Happy New Year', friend)
+print("Done")
---
+++ python_basics/app19_loops_finding_largest.py
@@ -0,0 +1,2 @@
+# finding largest value
+
