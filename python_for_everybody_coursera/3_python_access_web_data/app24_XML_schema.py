212029086f86e5c9a3d1bde1baa24156e11afcba
allgoody <allgoodiesnet@gmailcom>
2022-04-15 09:35:20 +0300

    new

---
+++ python_for_everybody_coursera/3_python_access_web_data/app24_XML_schema.py
@@ -0,0 +1,9 @@
+# From internal format to XML/ JSON - serialization
+# XML - eXtensible Markup Language
+# xml - simple and complex elements/ NODES
+# xml - does not care about indenting
+# purpose is to share structured data
+# <start_tag Attribute="yes">Text Content</end_tag>
+# <self_closing_tag Attribute="yes" /> has no text
+# XML schema - description of legal format of XML document
+# if some piece of XML meets the schema specs - it is VALID
