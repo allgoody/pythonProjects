9c7f02fa2946c72150eedf4e0d6abd20dc9665ad
allgoody <allgoodiesnet@gmailcom>
2022-04-28 08:28:04 +0300

    new

--- python_for_everybody_coursera/4_objects_databases_python/app08_using_databases.py
+++ python_for_everybody_coursera/4_objects_databases_python/app08_using_databases.py
@@ -1,1 +1,22 @@
 # SQLite - embeded DB and built-in in Python
+
+# CREATE TABLE Users(
+# 	name VARCHAR(128),
+# 	email VARCHAR(128)
+# )
+
+# INSERT INTO Users(name,email)VALUES('Kristin','kf@umich.edu')
+
+# DELETE FROM Users WHERE email='testemail@email.com'
+
+# UPDATE Users SET name='Charles Xavier' WHERE email='csev@imich.edu'
+
+# Update Users Set email='charles@wheels.roll' Where name='Charles Xavier'
+
+# * - all columns
+
+# SELECT*FROM Users WHERE email='charles@wheels.roll'
+
+# SELECT*FROM Users
+
+# SELECT*FROM Users ORDER BY name
