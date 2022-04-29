a3329b7c4c96007118268f57ed8c6be7ba59b187
allgoody <allgoodiesnet@gmailcom>
2022-04-29 10:22:32 +0300

    new

---
+++ python_for_everybody_coursera/4_objects_databases_python/app11_data_model.py
@@ -0,0 +1,12 @@
+# Don't put same string data in twice - use relationship instead
+
+# Primary key - particular row / unique number
+
+# Eg: album's primary key ends up being Track's foreing key ( bc the track belongs to the album)
+
+# Make senseble name - eg:
+# album's primary key ID ( id ) could be in Track's foreing key ID ( album_id )
+
+# Logical key -  unique id to get item from outside
+
+# Table - Primary key - Logical key - Foreing key
